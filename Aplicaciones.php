12ed05fe57c6cf4e4e55be351909f89eb8c88d01
xxmaucoxx <btr_kirito1@hotmail.com>
2016-07-07 09:20:55 -0500

    new
    aq

---
+++ Aplicaciones.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Aplicaciones - Runa IT</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="/">Runa IT</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="/">Inicio</a></li>
+											<li><a href="Aplicaciones">Aplicaciones</a></li>
+											<li><a href="Nosotros">Nosotros</a></li>
+											<li><a href="Contacto">Contacto</a></li>
+
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Nuestras Aplicaciones</h2>
+						</header>
+						<section class="wrapper style5">
+							<div class="inner">
+
+								<h3>RunaEduca</h3>
+								<p>Aplicación dedicada a la planificación y seguimiento pedagógico en instituciones educativas.</p>
+								<p>La correcta planificación curricular en tiempo y forma es un elemento importante del éxito escolar, por esto es importante tener los recursos y herramientas necesarios para realizar esta planificación y hacer el seguimiento correspondiente para medir los resultados del avance de la misma.</p>
+								<hr />
+								<h3>RunaHotel</h3>
+								<p>Aplicación dedicada a ventas y administración de reservas hoteleras, totalmente configurable.</p> 
+								<p>La importancia de tener un punto de venta online (24 x 365) accesible desde cualquier punto del planeta que pueda en todo momento estar actualizado sin necesidad de conocimientos de programación, así como el control de ocupación del mismo en forma local o remota, le otorga la versatilidad necesaria para tomar decisiones y tener un completo control sobre las operaciones</p>
+								<hr />
+
+								<h3>RunaTour</h3>
+								<p> Aplicación dedicada al desarrollo turístico construida utilizando lo último en tecnología de realidad aumentada.</p>
+								<p>Conocer los lugares turísticos de cada ciudad mirando imágenes “casi reales” que nos transporten al pasado para poder observar en primera persona las ocurrencias y situaciones de la época es sin duda una nueva forma de hacer turismo “vivencial”.</p>
+								<hr />
+								<h3>RunaCare</h3>
+								<p> Aplicación dedicada a la gestión de turnos, atención clínica y administración de centros de salud.</p>
+								<p>Tener a disposición un sistema de gestión de turnos en linea (24 x 365) además de tener en la punta de los dedos la información necesaria para la toma de decisiones (tableros de comando,  indicadores clave, etc.) es una herramienta poderosa que mas de un gerente de operaciones médicas desearía tener a disposición.</p>
+								<hr />
+								
+							</div>
+						</section>
+					</article>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<meta name="description" content="La pagina coorporativa de Runa IT , es el punto de entrada de la inforción de productos y servicios que está ofrece" />
+		<meta name="keywords" content="Runait Runa IT tecnológica desarrollo moquegua javier coaila tecnologia " />
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="author" content="Javier Coaila">
+		<meta name="robots" content="index, follow">
+		<meta name="revisit-after" content="3 month">
+		<title>Runa IT</title>
+		<link rel="icon" href="favicon.ico" type="image/x-icon" />
+		
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/kira.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="/">Runa IT</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="/">Inicio</a></li>
+											<li><a href="Nosotros">Nosotros</a></li>
+											<li><a href="Aplicaciones">Aplicaciones</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<img class="logoIco" src="images/logo.png" alt="" height="70" width="90">
+						<div class="inner">
+							<h2><img src="images/titulo.png" width="220" alt=""></h2>
+							<p>Desarrollo de soluciones<br /> 
+							para el crecimiento e innovación<br />
+							de la sociedad.</p>
+							<ul class="actions">
+								<li><a href="#" class="button special">Contactanos</a></li>
+							</ul>
+						</div>
+						<a href="#one" class="more scrolly">Más información</a>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper  special" style="background-color:#3A539B">
+						<div class="inner">
+							<header class="major">
+								<h2>Arcu aliquet vel lobortis ata nisl<br />
+								eget augue amet aliquet nisl cep donec</h2>
+								<p>Llegar a ser un aliado del desarrollo de cada una de las personas de la sociedad<br />
+								 tanto a nivel personal como a nivel empresarial.</p>
+							</header>
+							<ul class="icons major">
+								<li><span class="icon fa-diamond major style1" style="color: white"><span class="label">Lorem</span></span></li>
+								<li><span class="icon fa-heart-o major style2" style="color: white"><span class="label">Ipsum</span></span></li>
+								<li><span class="icon fa-code major style3" style="color: white"><span class="label">Dolor</span></span></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<section class="spotlight">
+							<div class="image"><img src="images/img1.png" alt="" /></div><div class="content">
+								<h2>Desarrollo Web</h2>
+								<p>Todas las páginas web existentes utilizan HTML para poder mostrar la información de manera adecuada junto con CSS el cual se encarga de definir colores, tamaños, ubicaciones de los elementos, por ultimo se incluye jQuery (framework de JavaScript) para dar animación al diseño web</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/img3.jpg" alt="" /></div><div class="content">
+								<h2>Adaptable a dispositivos móviles</h2>
+								<p>Responde a las necesidades de los usuarios y de sus dispositivos. El diseño cambia en función del tamaño y de la capacidad del dispositivo.</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/img2.jpg" alt="" /></div><div class="content">
+								<h2>Desarrollo de aplicaciones moviles</h2>
+								<p>Somos especialistas en desarrollo de soluciones móviles a medida para su empresa. El potencial tecnológico de los dispositivos móviles hoy en día nos permite desarrollar prácticamente cualquier aplicación móvil que pueda imaginar.</p>
+							</div>
+						</section>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Nuestras Aplicaciones</h2>
+								<p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
+								fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+							</header>
+							<ul class="features">
+								<li class="icon fa-paper-plane-o">
+									<h3>RunaEduca</h3>
+									<p>Aplicación dedicada a la planificación y seguimiento pedagógico en instituciones educativas.</p>
+								</li>
+								<li class="icon fa-code">
+									<h3>RunaHotel</h3>
+									<p>Aplicación dedicada a ventas y administración de reservas hoteleras, totalmente configurable.</p>
+								</li>
+								<li class="icon fa-headphones">
+									<h3>RunaTour</h3>
+									<p> Aplicación dedicada al desarrollo turístico construida utilizando lo último en tecnología de realidad aumentada</p>
+								</li>
+								<li class="icon fa-flag-o">
+									<h3>RunaCare</h3>
+									<p> Aplicación dedicada a la gestión de turnos, atención clínica y administración de centros de salud.</p>
+								</li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+							<header>
+								<h2>Arcue ut vel commodo</h2>
+								<p>Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet eleifend fringilla.</p>
+							</header>
+							<ul class="actions vertical">
+								<li><a href="#" class="button fit special">Activate</a></li>
+								<li><a href="#" class="button fit">Learn More</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="https://www.facebook.com" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
